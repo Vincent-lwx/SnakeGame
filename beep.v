600ca879a7a0c1845c5592b2d9baa3e3849ccfb1
Vincent-lwx <vincentliu0706@gmail.com>
2024-12-28 15:51:34 +0800

    Update beep.v

--- beep.v
+++ beep.v
@@ -38,45 +38,6 @@ always @(posedge sys_clk or negedge sys_rst_n) begin
 end
 
 
-/*
-always @(posedge sys_clk or negedge sys_rst_n) begin 
-    if (!sys_rst_n) begin 
-        delay_cnt <= 32'd0;
-    end
-    else begin
-        if(enb)// 如果enb信号为高
-            delay_cnt <= 32'd100000000; //给延时计数器重新装载初始值（计数时间为1s）
-        else  
-        begin   //在按键状态稳定时，计数器递减，开始1ms倒计时
-                 if(delay_cnt > 32'd0)
-                     delay_cnt <= delay_cnt - 1'b1;
-                 else
-                     delay_cnt <= delay_cnt;
-        end				 
-    end   
-end
-
-
-
-// 控制嗡鸣器使能信号的输出
-always @(posedge sys_clk or negedge sys_rst_n) begin 
-    if (!sys_rst_n) begin 
-        beep_en <= 1'b0;          
-    end
-    else begin
-	    if(enb) // 如果enb信号为高，则立即设置嗡鸣器使能信号为高
-		    beep_en <= 1'b1;
-		else
-        if(delay_cnt == 32'd1) begin   //当计数器递减到1时，说明状态维持了1s
-            beep_en <= 1'b0;  // 将嗡鸣器使能信号设置为低，关闭嗡鸣器         
-        end
-        else begin
-            beep_en <= beep_en; 
-        end  
-    end   
-end
-
-*/
 
 
 endmodule 
