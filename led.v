cd397321b743d5e16b5b26fa9ca1095e4a8e50d2
Vincent-lwx <vincentliu0706@gmail.com>
2024-12-28 15:57:22 +0800

    Update led.v

--- led.v
+++ led.v
@@ -8,24 +8,7 @@ module led(
 // 定义一个计时器来控制LED的亮起时间
 reg [21:0] led_timer; 
 
-/*
-// LED模块的行为描述
-always @(posedge sys_clk or negedge sys_rst_n) begin
-    if (!sys_rst_n) begin
-        led <= 4'b0000;            // 初始状态LED熄灭
-        led_timer <= 0;
-    end else begin
-        if (trigger) begin    // 如果接收到触发信号
-            led <= 4'b1111;        // LED点亮
-            led_timer <= (32000000 / 2); // 32MHz时钟频率下，设置计时器为0.5秒
-        end else if (led && led_timer != 0) begin
-            led_timer <= led_timer - 1; // 开始倒计时
-            if (led_timer == 0) led <= 0; // 计时结束，LED熄灭
-        end
-    end
-end
 
-*/
 
 
 
